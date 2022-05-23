6426b3a2d0ec6ca57a968e298d9b659e3721db06
Ido Ben-Hur <idoybh2@gmail.com>
2022-05-23 14:16:48 +0300

    Add car conversion list
    Co-authored-by: OmriShemn <omri.shemen1994@gmail.com>

--- scrape.py
+++ scrape.py
@@ -1,5 +1,4 @@
 import os
-
 import requests
 import random
 import numpy as np
@@ -8,6 +7,64 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import seaborn as sns
 from bs4 import BeautifulSoup as bs
+
+carNamesArr = [
+    ["אאודי","Audi"],
+    ["אברת\'","Abarth"],
+    ["אופל","Opel"],
+    ["איוויס","Aiways"],
+    ["אינפיניטי","Infiniti"],
+    ["איסוזו","Isuzu"],
+    ["אלפא רומאו","Alpha Romeo"],
+    ["אם. ג\'י. / MG","MG"],
+    ["אסטון מרטין","Aston-Martin"],
+    ["ב.מ.וו","BMW"],
+    ["בנטלי","Bently"],
+    ["ג\'י.איי.סי/ GAC","GAC"],
+    ["ג\'ילי - Geely","Geely"],
+    ["ג\'יפ / Jeep","Jeep"],
+    ["ג\'נסיס","Genesis"],
+    ["דאצ\'יה","Dacia"],
+    ["דודג\'","Dodge"],
+    ["דונגפנג","Dongfeng"],
+    ["די.אס / DS","DS Automobiles"],
+    ["הונדה","Honda"],
+    ["הינו \ HINO","Hino"],
+    ["וולוו","Volvo"],
+    ["טויוטה","Toyota"],
+    ["טסלה","Tesla"],
+    ["יגואר","Jaguar"],
+    ["יונדאי","Hyundai"],
+    ["למבורגיני","Lamborghini"],
+    ["לנד רובר","Land Rover"],
+    ["לקסוס","Lexus"],
+    ["מאזדה ","Mazda"],
+    ["מאן","MAN"],
+    ["מיני","Mini"],
+    ["מיצובישי","Mitsubishi"],
+    ["מקסוס","Maxus"],
+    ["מרצדס","Mercedes-Benz"],
+    ["ניסאן","Nisssan"],
+    ["סאנגיונג","SsangYong"],
+    ["סובארו","Subaru"],
+    ["סוזוקי","Suzuki"],
+    ["סיאט","Seat"],
+    ["סיטרואן","Citroen"],
+    ["סמארט","Smart"],
+    ["סקודה","Skoda"],
+    ["סרס / SERES","Seres"],
+    ["פולסווגן","Volkswagen"],
+    ["פורד","Ford"],
+    ["פורשה","Porsche"],
+    ["פיאט","Fiat"],
+    ["פיג\'ו","Peugeot"],
+    ["קאדילק","Cadillac"],
+    ["קופרה","Cupra"],
+    ["קיה","Kia"],
+    ["קרייזלר","Chrysler"],
+    ["רנו","Renault"],
+    ["שברולט","Chevrolet"],
+]
 
 # scraping the price list
 
