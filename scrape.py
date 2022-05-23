2704aff58619535875d59b817e169cf06bbfcf96
Ido Ben-Hur <idoybh2@gmail.com>
2022-05-23 14:17:12 +0300

    Avoid bot detection
    Co-authored-by: OmriShemn <omri.shemen1994@gmail.com>

--- scrape.py
+++ scrape.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import seaborn as sns
 from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 
 carNamesArr = [
     ["אאודי","Audi"],
@@ -66,15 +69,23 @@ carNamesArr = [
     ["שברולט","Chevrolet"],
 ]
 
-# scraping the price list
-
 purl="https://pricelist.yad2.co.il/"
-rndVersion = random.randint(0,9223372036854775807)
-agent = 'Mozilla/' + str(rndVersion) + ' (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'
-headers = {'User-Agent': agent}
-newCarsUrls=()
-pPage = requests.get(purl, headers=headers)
-requests.session().cookies.clear()
-pHTML = bs(pPage.text, 'html.parser')
-r = pHTML.find_all('td', {'class': 'CarManufactur'})
-print(pHTML)
+carUrlFormat="https://pricelist.yad2.co.il/search.php?ExceptionValue=%s&Exception=CarManufactur"
+
+content=""
+while True:
+    options = Options()
+    options.binary_location = r'/usr/bin/firefox-developer-edition'
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
+    driver.get(purl)
+    content = driver.page_source
+    if (content.find("Captcha Digest:") != -1):
+        print("Bot detected. Please solve the CAPTCHA")
+        options.headless = False
+        driver = webdriver.Firefox(options=options)
+        driver.get(purl)
+        input("Press Enter after solving to continue")
+    else:
+        break;
+print(content)
