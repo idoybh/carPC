e61f66fb42313cfdab3111c20433066ec6a4117a
Ido Ben-Hur <idoybh2@gmail.com>
2022-05-29 16:47:39 +0300

    Convert car names to a hash table
    And ask if we want to rebuild the DB
    
    Co-authored-by: OmriShemn <omri.shemen1994@gmail.com>

--- scrape.py
+++ scrape.py
@@ -12,63 +12,63 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 
-carNamesArr = [
-    ["אאודי","Audi"],
-    ["אברת\'","Abarth"],
-    ["אופל","Opel"],
-    ["איווייס","Aiways"],
-    ["אינפיניטי","Infiniti"],
-    ["איסוזו","Isuzu"],
-    ["אלפא רומיאו","Alpha Romeo"],
-    ["אם. ג\'י. / MG","MG"],
-    ["אסטון מרטין","Aston-Martin"],
-    ["ב.מ.וו","BMW"],
-    ["בנטלי","Bently"],
-    ["ג\'י.איי.סי/ GAC","GAC"],
-    ["ג\'ילי - Geely","Geely"],
-    ["ג\'יפ / Jeep","Jeep"],
-    ["ג\'נסיס","Genesis"],
-    ["דאצ\'יה","Dacia"],
-    ["דודג\'","Dodge"],
-    ["דונגפנג","Dongfeng"],
-    ["די.אס / DS","DS Automobiles"],
-    ["הונדה","Honda"],
-    ["הינו \ HINO","Hino"],
-    ["וולוו","Volvo"],
-    ["טויוטה","Toyota"],
-    ["טסלה","Tesla"],
-    ["יגואר","Jaguar"],
-    ["יונדאי","Hyundai"],
-    ["למבורגיני","Lamborghini"],
-    ["לנד רובר","Land Rover"],
-    ["לקסוס","Lexus"],
-    ["מאזדה","Mazda"],
-    ["מאן","MAN"],
-    ["מיני","Mini"],
-    ["מיצובישי","Mitsubishi"],
-    ["מקסוס","Maxus"],
-    ["מרצדס","Mercedes-Benz"],
-    ["ניסאן","Nisssan"],
-    ["סאנגיונג","SsangYong"],
-    ["סובארו","Subaru"],
-    ["סוזוקי","Suzuki"],
-    ["סיאט","Seat"],
-    ["סיטרואן","Citroen"],
-    ["סמארט","Smart"],
-    ["סקודה","Skoda"],
-    ["סרס / SERES","Seres"],
-    ["פולקסווגן","Volkswagen"],
-    ["פורד","Ford"],
-    ["פורשה","Porsche"],
-    ["פיאט","Fiat"],
-    ["פיג\'ו","Peugeot"],
-    ["קאדילק","Cadillac"],
-    ["קופרה","Cupra"],
-    ["קיה","Kia"],
-    ["קרייזלר","Chrysler"],
-    ["רנו","Renault"],
-    ["שברולט","Chevrolet"],
-]
+carNamesArr = {
+    "אאודי":"Audi",
+    "אברת\'":"Abarth",
+    "אופל":"Opel",
+    "איווייס":"Aiways",
+    "אינפיניטי":"Infiniti",
+    "איסוזו":"Isuzu",
+    "אלפא רומיאו":"Alpha Romeo",
+    "אם. ג\'י. / MG":"MG",
+    "אסטון מרטין":"Aston-Martin",
+    "ב.מ.וו":"BMW",
+    "בנטלי":"Bently",
+    "ג\'י.איי.סי/ GAC":"GAC",
+    "ג\'ילי - Geely":"Geely",
+    "ג\'יפ / Jeep":"Jeep",
+    "ג\'נסיס":"Genesis",
+    "דאצ\'יה":"Dacia",
+    "דודג\'":"Dodge",
+    "דונגפנג":"Dongfeng",
+    "די.אס / DS":"DS Automobiles",
+    "הונדה":"Honda",
+    "הינו \ HINO":"Hino",
+    "וולוו":"Volvo",
+    "טויוטה":"Toyota",
+    "טסלה":"Tesla",
+    "יגואר":"Jaguar",
+    "יונדאי":"Hyundai",
+    "למבורגיני":"Lamborghini",
+    "לנד רובר":"Land Rover",
+    "לקסוס":"Lexus",
+    "מאזדה":"Mazda",
+    "מאן":"MAN",
+    "מיני":"Mini",
+    "מיצובישי":"Mitsubishi",
+    "מקסוס":"Maxus",
+    "מרצדס":"Mercedes-Benz",
+    "ניסאן":"Nisssan",
+    "סאנגיונג":"SsangYong",
+    "סובארו":"Subaru",
+    "סוזוקי":"Suzuki",
+    "סיאט":"Seat",
+    "סיטרואן":"Citroen",
+    "סמארט":"Smart",
+    "סקודה":"Skoda",
+    "סרס / SERES":"Seres",
+    "פולקסווגן":"Volkswagen",
+    "פורד":"Ford",
+    "פורשה":"Porsche",
+    "פיאט":"Fiat",
+    "פיג\'ו":"Peugeot",
+    "קאדילק":"Cadillac",
+    "קופרה":"Cupra",
+    "קיה":"Kia",
+    "קרייזלר":"Chrysler",
+    "רנו":"Renault",
+    "שברולט":"Chevrolet",
+}
 
 engineTypeArr = {
     "בנזין": "Benzene",
@@ -107,82 +107,84 @@ def navigate(url):
             driverRunning=True
             break;
 
-navigate("https://pricelist.yad2.co.il/")
+ans = input("Do you want to rebuild new cars DB ? [y/N]: ")
+if (ans == "y"):
+    # create a list with car makers and page links respectively
+    navigate("https://pricelist.yad2.co.il/")
+    carLinksList=[]
+    for element in carNamesArr:
+        carLinksList.append(carNamesArr[element])
+        link = driver.find_element(By.LINK_TEXT, element).get_attribute('href')
+        link = link.replace("NewCars.php", "search.php")
+        carLinksList.append(link)
 
-# create a list with car makers and page links respectively
-carLinksList=[]
-for element in carNamesArr:
-    carLinksList.append(element[1])
-    link = driver.find_element(By.LINK_TEXT, element[0]).get_attribute('href')
-    link = link.replace("NewCars.php", "search.php")
-    carLinksList.append(link)
+    data_columns = ("Maker", "Year", "Model", "Gear", "Engine Type", "Engine Volume", "Horse Power", "Doors", "Seats", "Price")
+    newCarsDF = pd.DataFrame(columns=data_columns)
+    i=0
+    currMaker=""
+    for page in carLinksList:
+        if (i % 2 == 0):
+            currMaker=page
+        else:
+            # scraping each maker
+            navigate(page)
+            subModelLinks=[]
+            elements = driver.find_elements(By.CLASS_NAME, "SubModelLink")
+            for element in elements:
+                subModelLinks.append(element.get_attribute('href'))
+            for model in subModelLinks:
+                navigate(model)
+                currYear = driver.find_element(By.NAME, "Year").get_attribute('value')
+                currModel = driver.find_element(By.NAME, "Model").get_attribute('value')
+                currPrice = driver.find_element(By.NAME, "Price").get_attribute('value')
+                currGear = driver.find_element(By.NAME, "Price").get_attribute('value')
+                generalDetails = driver.find_element(By.CLASS_NAME, "car_general_details")
+                generalDetailsList = generalDetails.find_elements(By.CLASS_NAME, "value")
+                j=1
+                if ("תיבת הילוכים" in driver.page_source):
+                    currGear = generalDetailsList[j].text != "ידני"
+                    j=j+1
+                else:
+                    currGear = ""
+                if ("סוג מנוע" in driver.page_source):
+                    currEngineType = engineTypeArr[generalDetailsList[j].text]
+                    j=j+1
+                else:
+                    currEngineType = ""
+                if ("מספר דלתות" in driver.page_source):
+                    currDoors = int(generalDetailsList[j].text)
+                    j=j+1
+                else:
+                    currDoors = 0
+                if ("מספר מושבים" in driver.page_source):
+                    currSeats = int(generalDetailsList[j].text)
+                    j=j+1
+                else:
+                    currSeats = 0
+                if ("נפח מנוע" in driver.page_source):
+                    currVolume = int(generalDetailsList[j].text.replace(',', '').replace(" סמ\"ק", ""))
+                    j=j+1
+                else:
+                    currVolume = 0
+                if ("מספר כוחות סוס" in driver.page_source):
+                    currHorse = int(generalDetailsList[j].text.replace(" כ\"ס", ""))
+                    j=j+1
+                else:
+                    currHorse = 0
+                row = { "Maker" : currMaker,
+                        "Year" : currYear,
+                        "Model" : currModel,
+                        "Gear" : currGear,
+                        "Engine Type" : currEngineType,
+                        "Engine Volume" : currVolume,
+                        "Horse Power" : currHorse,
+                        "Doors" : currDoors,
+                        "Seats" : currSeats,
+                        "Price" : currPrice }
+                newCarsDF.loc[len(newCarsDF.index)] = row
+                newCarsDF.to_csv('NewCars.csv')
+        i=i+1
 
-data_columns = ("Maker", "Year", "Model", "Gear", "Engine Type", "Engine Volume", "Horse Power", "Doors", "Seats", "Price")
-newCarsDF = pd.DataFrame(columns=data_columns)
-i=0
-currMaker=""
-for page in carLinksList:
-    if (i % 2 == 0):
-        currMaker=page
-    else:
-        # scraping each maker
-        navigate(page)
-        subModelLinks=[]
-        elements = driver.find_elements(By.CLASS_NAME, "SubModelLink")
-        for element in elements:
-            subModelLinks.append(element.get_attribute('href'))
-        for model in subModelLinks:
-            navigate(model)
-            currYear = driver.find_element(By.NAME, "Year").get_attribute('value')
-            currModel = driver.find_element(By.NAME, "Model").get_attribute('value')
-            currPrice = driver.find_element(By.NAME, "Price").get_attribute('value')
-            currGear = driver.find_element(By.NAME, "Price").get_attribute('value')
-            generalDetails = driver.find_element(By.CLASS_NAME, "car_general_details")
-            generalDetailsList = generalDetails.find_elements(By.CLASS_NAME, "value")
-            j=1
-            if ("תיבת הילוכים" in driver.page_source):
-                currGear = generalDetailsList[j].text != "ידני"
-                j=j+1
-            else:
-                currGear = ""
-            if ("סוג מנוע" in driver.page_source):
-                currEngineType = engineTypeArr[generalDetailsList[j].text]
-                j=j+1
-            else:
-                currEngineType = ""
-            if ("מספר דלתות" in driver.page_source):
-                currDoors = int(generalDetailsList[j].text)
-                j=j+1
-            else:
-                currDoors = 0
-            if ("מספר מושבים" in driver.page_source):
-                currSeats = int(generalDetailsList[j].text)
-                j=j+1
-            else:
-                currSeats = 0
-            if ("נפח מנוע" in driver.page_source):
-                currVolume = int(generalDetailsList[j].text.replace(',', '').replace(" סמ\"ק", ""))
-                j=j+1
-            else:
-                currVolume = 0
-            if ("מספר כוחות סוס" in driver.page_source):
-                currHorse = int(generalDetailsList[j].text.replace(" כ\"ס", ""))
-                j=j+1
-            else:
-                currHorse = 0
-            row = { "Maker" : currMaker,
-                    "Year" : currYear,
-                    "Model" : currModel,
-                    "Gear" : currGear,
-                    "Engine Type" : currEngineType,
-                    "Engine Volume" : currVolume,
-                    "Horse Power" : currHorse,
-                    "Doors" : currDoors,
-                    "Seats" : currSeats,
-                    "Price" : currPrice }
-            newCarsDF.loc[len(newCarsDF.index)] = row
-            newCarsDF.to_csv('NewCars.csv')
-    i=i+1
+    print("Scraping the new cars DB is done")
 
-print("Scraping the new cars DB is done")
 driver.close();
